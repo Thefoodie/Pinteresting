dbf32ebf1d40a0f6a0bf87f1be4c45dddd34460c
Whit Johnson <wbjohnson.4@gmail.com>
2014-02-04 01:46:29 -0800

    Implemented Devise for Users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Pinteresting::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # required for heroku
+  # Note to set this to your actual host
+  config.action_mailer.default_url_option = { :host => 'trifit.org' } 
 end
